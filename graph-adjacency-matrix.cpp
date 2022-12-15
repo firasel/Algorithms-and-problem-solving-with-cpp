d84dd3f3b3c174ecfd321a3924098e86017329e2
firasel <mdrasel601390@gmail.com>
2022-12-15 17:27:19 +0600

    Graph create with user input using adjacency matrix

--- graph-adjacency-matrix.cpp
+++ graph-adjacency-matrix.cpp
@@ -3,26 +3,22 @@ using namespace std;
 
 int main()
 {
-  int n = 4;
-  vector<vector<int>> matrix(n, vector<int>(n, 0));
-
-  matrix[0][1] = 1;
-  matrix[1][0] = 1;
-
-  matrix[1][2] = 1;
-  matrix[2][1] = 1;
-
-  matrix[2][3] = 1;
-  matrix[3][2] = 1;
-
-  matrix[3][1] = 1;
-  matrix[1][3] = 1;
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> arr(n, vector<int>(n, 0));
+  for (int i = 0; i < m; i++)
+  {
+    int u, v;
+    cin >> u >> v;
+    arr[u][v] = 1;
+    arr[v][u] = 1;
+  }
 
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
     {
-      cout << matrix[i][j] << " ";
+      cout << arr[i][j] << " ";
     }
     cout << endl;
   }
