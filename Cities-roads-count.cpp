a45b7fdc2a1d7216ee5efe404384270f4d56f163
firasel <mdrasel601390@gmail.com>
2023-01-15 23:24:00 +0600

    Cities and roads count - solved

---
+++ Cities-roads-count.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, cnt = 0;
+  cin >> n;
+  int arr[n][n];
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      cin >> arr[i][j];
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      if (arr[i][j] == arr[j][i] && arr[i][j] == 1)
+        cnt++;
+
+  cout << cnt / 2 << endl;
+}
