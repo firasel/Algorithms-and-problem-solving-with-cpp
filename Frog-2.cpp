c906e2359888c3a0128dfed84a0dcd0040c11d82
firasel <mdrasel601390@gmail.com>
2023-02-09 00:23:17 +0600

    Frog-2 solved

---
+++ Frog-2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+const int INF = 2e9;
+
+int h[N], dp[N];
+int n, k;
+
+int stone_2(int n)
+{
+  if (n == 1)
+    return 0;
+
+  if (dp[n] != -1)
+    return dp[n];
+
+  int ans = INF;
+
+  for (int i = 1; i <= k; i++)
+  {
+    if (n - i <= 0)
+      break;
+    int candidate_ans = stone_2(n - i) + abs(h[n] - h[n - i]);
+    ans = min(ans, candidate_ans);
+  }
+
+  dp[n] = ans;
+  return ans;
+}
+
+int main()
+{
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++)
+    cin >> h[i];
+
+  for (int i = 1; i <= n; i++)
+    dp[i] = -1;
+
+  cout << stone_2(n) << endl;
+
+  return 0;
+}
